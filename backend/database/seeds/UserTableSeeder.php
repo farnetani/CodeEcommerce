90b412cba5fe598880658b7839d074fd45e173c8
farnetani <farnetani@gmail.com>
2015-05-02 19:13:19 -0300

    Criado as classes de Seed : Categorias e Usuários.

---
+++ backend/database/seeds/UserTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use CodeCommerce\User;
+use Faker\Factory as Faker;
+
+class UserTableSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('users')->truncate();
+
+		$faker = Faker::create();
+
+		User::create([
+			'name' => 'Junior',
+			'email' => 'farnetani@gmail.com',
+			'password' => Hash::make(123456),
+		]);
+
+		foreach(range(1,10) as $i) {
+			User::create([
+				'name' => $faker->name(),
+				'email'=> $faker->email(),
+				'password' => Hash::make($faker->word)
+			]);
+		}
+
+	}
+}
